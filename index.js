37475bfd8ab0a83631d64e86f18e1267f21cabbc
imranhasan2 <imranhasan030303@gmail.com>
2024-06-09 02:25:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors =require('cors')
+require('dotenv').config()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('tour is running')
+})
+
+app.listen(port, () => {
+  console.log(`tour planner is listening on port: ${port}`)
+})
