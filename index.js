dae791b537e596adb798ab637853a7333c790112
imranhasan2 <imranhasan030303@gmail.com>
2024-06-11 00:25:31 +0600

    getting data from server for guide

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhkveen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,13 +31,45 @@ async function run() {
 
 
         const packageCollection = client.db('tourDb').collection('package')
+        const wishCollection = client.db('tourDb').collection('wishList')
+        const guideCollection = client.db('tourDb').collection('guide')
 
 
 
+        // guide
+        app.get('/guides',async(req,res) => {
+            const result=await guideCollection.find().toArray()
+            res.send(result)
+        })
 
 
-        app.get('/package',async(req,res) =>{
-            const result =await packageCollection.find().toArray()
+        // packages
+
+        app.get('/package', async (req, res) => {
+            const result = await packageCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        app.get('/packageDetails/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await packageCollection.findOne(query)
+            res.send(result)
+        })
+
+        // wishList 
+
+
+        app.get('/wishList', async (req, res) => {
+            const result = await wishCollection.find().toArray()
+            res.send(result)
+
+        })
+
+        app.post('/wishList', async (req, res) => {
+            const item = req.body;
+            const result = await wishCollection.insertOne(item)
             res.send(result)
         })
 
