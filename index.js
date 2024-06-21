736c9ff6af05b039f9bc7008e0b39d5f9cc00aee
imranhasan2 <imranhasan030303@gmail.com>
2024-06-21 20:17:21 +0600

    added the admin and guide api

--- index.js
+++ index.js
@@ -34,22 +34,61 @@ async function run() {
 
 
         // user Collection
-        app.post('/users',async(req,res) =>{
-            const user =req.body
+        app.post('/users', async (req, res) => {
+            const user = req.body
 
-            const query ={email : user?.email}
+            const query = { email: user?.email }
 
             const existingUser = await userCollection.findOne(query)
-            if(existingUser){
-                return res.send({message:'user already exist',insertedId:null})
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedId: null })
             }
 
-            const result =await userCollection.insertOne(user)
+            const result = await userCollection.insertOne(user)
+
+            res.send(result)
+        })
+
+        app.get('/users',async(req,res) =>{
+            const user =req.body;
+            const result =await userCollection.find(user).toArray()
 
             res.send(result)
         })
 
 
+
+
+
+        // admin && guide
+
+        app.patch('/users/guide/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+
+            const updateDoc = {
+                $set: {
+                    role: 'guide'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
 
 
 
